85759158d0ddbd42c358d5453fbc1de270c6a531
Eshraq Ibrahim <eshraq.ibrahim@gmail.com>
2021-02-25 14:24:33 +0200

    Fix missing import

--- uploadservice/src/main/java/net/gotev/uploadservice/exceptions/Exceptions.kt
+++ uploadservice/src/main/java/net/gotev/uploadservice/exceptions/Exceptions.kt
@@ -2,6 +2,7 @@ package net.gotev.uploadservice.exceptions
 
 import net.gotev.uploadservice.UploadThrowable
 import java.lang.Exception
+import net.gotev.uploadservice.network.ServerResponse
 
 class UserCancelledUploadException(httpMethod: String, exception: Exception? = null) : UploadThrowable(message = "User cancelled upload", httpMethod = httpMethod, exception)
 class UploadError(val serverResponse: ServerResponse, httpMethod: String, exception: Exception? = null) : UploadThrowable(message = "Upload error", httpMethod = httpMethod, exception = exception)
